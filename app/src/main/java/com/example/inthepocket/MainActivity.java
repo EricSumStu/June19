605447dc81b54d97693821e208e482a8b60dbbc9
EZMCCME <51365089+EZMCCME@users.noreply.github.com>
2019-06-11 15:43:26 +0100

    user input change
    button

--- app/src/main/java/com/example/inthepocket/MainActivity.java
+++ app/src/main/java/com/example/inthepocket/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.inthepocket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String name;
+    Button button;
+
+    EditText nameInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameInput = (EditText) findViewById(R.id.NameInput);
+
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                name = nameInput.getText().toString();
+
+                showToast(name);
+
+            }
+        });
+    }
+
+    private void showToast(String text) {
+        Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
     }
 }
