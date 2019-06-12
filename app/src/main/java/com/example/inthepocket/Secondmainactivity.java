8f0812f22d3746ac77d577403ff3924eb3d28aee
LukeHamilton3 <51365382+LukeHamilton3@users.noreply.github.com>
2019-06-12 10:14:13 +0100

    Homepage, app icon, logo

---
+++ app/src/main/java/com/example/inthepocket/Secondmainactivity.java
@@ -0,0 +1,14 @@
+package com.example.inthepocket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+class Secondmainativity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
