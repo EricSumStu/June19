ce72c1bf9ae587f392b2f144236efb62e84c29b6
eptrrmn <armen.petrosyan@ericsson.com>
2019-06-13 14:47:24 +0100

    code Fix V6

--- app/src/main/java/com/example/inthepocket/MainActivity.java
+++ app/src/main/java/com/example/inthepocket/MainActivity.java
@@ -3,21 +3,16 @@ package com.example.inthepocket;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
-
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button = (Button) findViewById(R.id.button);
 
     }
     public void onClick(View view) {
--- app/src/main/java/com/example/inthepocket/UserInput.java
+++ app/src/main/java/com/example/inthepocket/UserInput.java
@@ -2,67 +2,69 @@ package com.example.inthepocket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.util.ArrayList;
 
 public class UserInput extends AppCompatActivity {
-
-    public static final String EXTRA_MESSAGE = "com.example.InThePocket.MESSAGE";
-
+    public static int counter = 0;
     String name;
     Button button;
-
     EditText nameInput;
-    TextView displayName;
+    ListView arrayDisplay;
+    TextView displayCounter;
+    ArrayList<String> Array = new ArrayList<String>();
 
-    private void showToast(String text) {
-        Toast.makeText(UserInput.this, text, Toast.LENGTH_SHORT).show();
+    //going from page to page
+    public void onClick1(View view) {
+        startActivity(new Intent(UserInput.this, MainActivity.class));
     }
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_input);
 
         nameInput = (EditText) findViewById(R.id.NameInput);
-        displayName = (TextView) findViewById(R.id.editText);
-
+        displayCounter = (TextView) findViewById(R.id.CounterNumber);
+        arrayDisplay = (ListView) findViewById(R.id.listView);
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
-
-
             @Override
-            public void onClick(View v) {
-                name = nameInput.getText().toString();
+            public void onClick(View view) {
 
-                showToast(name);
-                displayName.setText(name);
+                 name = nameInput.getText().toString();
 
+                if (Array.contains(name)) {
+                    Toast.makeText(getBaseContext(), "Username taken", Toast.LENGTH_LONG).show();
+                } else if (name == null || name.trim().equals("")) {
+                    Toast.makeText(getBaseContext(), "ItemField Is Empty", Toast.LENGTH_LONG).show();
+                } else if (counter == 10) {
+                    Toast.makeText(UserInput.this, "Tournament full!!", Toast.LENGTH_LONG).show();
+                } else {
+                    if (counter < 10){
+                        counter++;
+                    }else{
+                        counter = 10;
+                    }
+                    Array.add(name);
+                    ArrayAdapter<String> adapter = new ArrayAdapter<>(UserInput.this, android.R.layout.simple_list_item_1, Array);
+                    arrayDisplay.setAdapter(adapter);
+                    ((EditText) findViewById(R.id.NameInput)).setText("");
+                }
+                displayCounter.setText("Players: " + counter);
             }
         });
     }
-
-    public void onClick1(View view) {
-        startActivity(new Intent(UserInput.this, MainActivity.class));
-    }
 }
-
-/*
-
-public class MyClass {
-  public static void main(String[] args) {
-    ArrayList<String> players = new ArrayList<String>();
-    players.add(message)
-    System.out.println(message);//ArrayList
-  }
-}
-
- */
 
 
 
