3994cca57321eca2a9f09faee12ad280ea3f2502
LukeHamilton3 <51365382+LukeHamilton3@users.noreply.github.com>
2019-06-24 10:06:26 +0100

    Create StartTournament.java

---
+++ app/src/main/java/com/example/inthepocket/StartTournament.java
@@ -0,0 +1,15 @@
+package com.example.inthepocket;
+
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartTournament extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_tournament);
+    }
+}
