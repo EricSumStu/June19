905c3a75d4fd3d95ba2a45ae14a9b2262d34b504
clemass <41894951+clemass@users.noreply.github.com>
2019-06-25 15:31:43 +0100

    Delete buttons now have a function !! also its just 10 times better :)

--- app/src/main/java/com/example/inthepocket/UserCustomAdapter.java
+++ app/src/main/java/com/example/inthepocket/UserCustomAdapter.java
@@ -10,34 +10,33 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class UserCustomAdapter extends ArrayAdapter<User> {
+public class UserCustomAdapter extends ArrayAdapter<String> {
 
-
+    private static OnDataChangeListener mOnDataChangeListener;
     Context context;
     int layoutResourceId;
-    ArrayList<User> userArray = new ArrayList<User>();
+    ArrayList<String> userArray = new ArrayList<String>();
     ListView arrayDisplay;
+   // TextView displayCounter;
 
 
-    public UserCustomAdapter(Context context, int layoutResourceId, ArrayList<User> String) {
+    public UserCustomAdapter(Context context, int layoutResourceId, ArrayList<String> String) {
         super(context, layoutResourceId, String);
         this.layoutResourceId = layoutResourceId;
         this.context = context;
         this.userArray = String;
     }
 
-
-
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
         View row = convertView;
         UserHolder holder = null;
-
 
         if (row == null) {
             LayoutInflater inflater = ((Activity) context).getLayoutInflater();
@@ -50,36 +49,34 @@ public class UserCustomAdapter extends ArrayAdapter<User> {
             holder = (UserHolder) row.getTag();
         }
 
-        User user = userArray.get(position);
-        holder.textName.setText(user.getName());
-        final View finalRow = row;
+        final String user = userArray.get(position);
+        holder.textName.setText(user);
         holder.btnDelete.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
                 Log.i("Delete Button Clicked", "**********");
-                Toast.makeText(context, "Delete button Clicked- tough luck learn to live with your mistakes " + v.findViewById(R.id.button),
+                Toast.makeText(context,  user +" has been deleted",
                         Toast.LENGTH_LONG).show();
-                Button deleteButton = (Button) finalRow.findViewById(R.id.button2);
-                deleteButton.setTag(position);
 
-                deleteButton.setOnClickListener(
-                        new Button.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                               // Integer index = (Integer) arrayDisplay.getTag();
-                         //       userArray.remove(index.intValue());
-                             //   Array.remove(index.intValue());
-                                //notifyDataSetChanged();
-                            }
-                        }
-                );
-                //System.out.println(userArray);
+                userArray.remove(user);
+                notifyDataSetChanged();
+                if(mOnDataChangeListener != null){
+                    mOnDataChangeListener.onDataChanged(userArray.size());
+                }
+                //displayCounter.setText("Players: " + userArray.size());
+
+
             }
         });
         return row;
-
+    }
+    public interface OnDataChangeListener{
+        public void onDataChanged(int size);
+    }
+    public static void setOnDataChangeListener(OnDataChangeListener onDataChangeListener){
+        mOnDataChangeListener = onDataChangeListener;
     }
 
 
@@ -88,32 +85,3 @@ public class UserCustomAdapter extends ArrayAdapter<User> {
         Button btnDelete;
     }
 }
-
-
-
-
-/*
-    Button deleteBtn = (Button)view.findViewById(R.id.delete_btn);
-    Button addBtn = (Button)view.findViewById(R.id.add_btn);
-
-    deleteBtn.setOnClickListener(new View.OnClickListener(){
-        @Override
-        public void onClick(View v) {
-            //do something
-            list.remove(position); //or some other task
-            notifyDataSetChanged();
-        }
-    });
-
-
-    public void delete(View v){
-
-    ListView listview1;
-    ArrayList<E> datalist;
-
-    final int position = listview1.getPositionForView((View) v.getParent());
-    datalist.remove(position);
-    myAdapter.notifyDataSetChanged();
-
-}
- */
